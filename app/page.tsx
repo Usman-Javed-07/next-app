d79537be7f9c409250ae21f20c7ea2d275e79bf1
Usman Javed <usmanjaved0816@gmail.com>
2025-05-18 15:12:47 +0500

    fetching data form api in next js

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ProductCard from './components/ProductCard'
 export default function Home() {
   return (
     <main>
-<h1>Hello next js</h1>
+    <h1>Hello next js</h1>
    <Link  href="/users">users</Link>
    <ProductCard/>
     </main>
